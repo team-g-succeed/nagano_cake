fb77c4b090cb0a865aaa7cbc26edaf58be4b637f
EC2 Default User <ec2-user@ip-172-31-42-87.ap-northeast-3.compute.internal>
2024-04-27 04:53:23 +0000

    ちょっとした修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,10 +1,4 @@
 class Public::HomesController < ApplicationController
-<<<<<<< Updated upstream
-  def top
-    #@items = Item.page(params[:page]).per(4).order(created_at: :desc)
-  end
-  def about
-=======
  def top
     @items = Item.order(created_at: :desc).page(params[:page]).limit(4)
     @genres = Genre.all
@@ -12,7 +6,6 @@ class Public::HomesController < ApplicationController
 
  def about
  end
->>>>>>> Stashed changes
 
-  end
+
 end
